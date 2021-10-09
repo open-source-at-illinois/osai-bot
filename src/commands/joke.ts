0e5f84a7d070e1eda3c0646a1828e3205960f508
Raul <raul.d.higareda@gmail.com>
2021-10-09 17:41:39 -0500

    Ensure JSON.parse is called only once

--- src/commands/joke.ts
+++ src/commands/joke.ts
@@ -14,9 +14,8 @@ module.exports = {
         const response = await got('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit');
 
         // Parse String response into JSON
-        const setup = JSON.parse(response.body).setup;
-        const delivery = JSON.parse(response.body).delivery;
-
-        await interaction.reply(setup + "\n" + delivery);
+        const body = JSON.parse(response.body);
+        
+        await interaction.reply(body.setup + "\n" + body.delivery);
     },
 };
