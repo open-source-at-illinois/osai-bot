0034e84712db26170afac236099f07ac1ccddc9b
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-05 13:51:15 -0500

    Add OSAI guild and role IDs

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,9 @@ export const VERIFY_TOKEN_LENGTH = 5;
 // Expiration time for verification tokens in milliseconds
 // Currently, 30 minutes
 export const VERIFY_TOKEN_EXPIRATION = 30 * 60 * 1000;
+
+export const OSAI_GUILD_ID = '622246265693929482';
+
+export const EXEC_ROLE_ID = '622251319457218591'; 
+
+export const OSAI_ROLE_ID = '983068543543312444'; 
