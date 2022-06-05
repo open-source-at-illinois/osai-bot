3c5e609348d7289db7c0209b0900de8521ce1f89
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-05 16:52:10 -0500

    Use commonJS import for mongoose

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     netid: {
--- src/models/verification.ts
+++ src/models/verification.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose = require('mongoose');
 
 const verificationSchema = new mongoose.Schema({
     discordId: {
