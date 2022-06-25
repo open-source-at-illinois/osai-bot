1d620f89423a83b2d739449efdb6312f6e6298da
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-24 22:55:44 -0500

    Format file

--- index.ts
+++ index.ts
@@ -1,39 +1,43 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-require('dotenv').config()
-const fs = require('fs');
-const mongoose = require('mongoose');
-const { Client, Intents, Collection } = require('discord.js');
+require("dotenv").config();
+const fs = require("fs");
+const mongoose = require("mongoose");
+const { Client, Intents, Collection } = require("discord.js");
 import type { CommandInteraction } from "discord.js";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-  console.log('OSAI-Bot is Ready!');
+client.once("ready", () => {
+  console.log("OSAI-Bot is Ready!");
 });
 
 client.commands = new Collection();
 
 // TODO recursively get all directories in the commands folder
-const commandDirs = ['fun', 'logistics', 'util'];
+const commandDirs = ["fun", "logistics", "util"];
 
 for (const dir of commandDirs) {
-  const commandFiles = fs.readdirSync(`./src/commands/${dir}`).filter((file: string) => file.endsWith('.ts') || file.endsWith('.js'));
+  const commandFiles = fs
+    .readdirSync(`./src/commands/${dir}`)
+    .filter((file: string) => file.endsWith(".ts") || file.endsWith(".js"));
   for (const file of commandFiles) {
     const command = require(`./src/commands/${dir}/${file}`);
     client.commands.set(command.data.name, command);
   }
 }
 
-
-client.on('interactionCreate', async (interaction: CommandInteraction) => {
+client.on("interactionCreate", async (interaction: CommandInteraction) => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
   if (!command) return;
 
-  await command.execute(interaction).catch(async error => {
+  await command.execute(interaction).catch(async (error) => {
     console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   });
 });
 
