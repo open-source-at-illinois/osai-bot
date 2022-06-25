49d785fd2cb5b638ed49ecf8fdeee4580a505822
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-24 23:50:54 -0500

    Add hard delete option to /endevent

--- src/commands/logistics/endEvent.ts
+++ src/commands/logistics/endEvent.ts
@@ -5,32 +5,51 @@ import Event from "../../models/event";
 import { isExec } from "../../utils";
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('endevent')
-        .setDescription('Ends an event by marking it as inactive')
-        .addStringOption((option) => option.setName('code').setDescription('Event code').setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName("endevent")
+    .setDescription("Ends an event by marking it as inactive")
+    .addStringOption((option) =>
+      option.setName("code").setDescription("Event code").setRequired(true)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("hard")
+        .setDescription("Deletes event from database. This cannot be undone!")
+        .setRequired(false)
+    ),
+  async execute(interaction: CommandInteraction) {
+    const code = interaction.options.getString("code").trim();
+    const hardDelete = interaction.options.getBoolean("hard");
 
-    async execute(interaction: CommandInteraction) {
-        const code = interaction.options.getString('code').trim();
-
-        if (!isExec(interaction.member as GuildMember)) {
-            interaction.reply(NOT_AUTHORIZED_MESSAGE);
-            return;
-        }
-
-        // Find an event with the given code
-        const event = await Event.findOne({ code }).exec();
-        if (!event) {
-            interaction.reply(`Could not find an event with code ${code}`);
-            return;
-        }
-        if (!event.active) {
-            interaction.reply(`This event was already inactive.`);
-            return;
-        }
-
-        event.active = false;
-        await event.save();
-        interaction.reply(`Event ${event.name} has been marked as inactive, and can no longer be attended.`);
+    if (!isExec(interaction.member as GuildMember)) {
+      interaction.reply(NOT_AUTHORIZED_MESSAGE);
+      return;
     }
-}
+
+    // Find an event with the given code
+    const event = await Event.findOne({ code }).exec();
+    if (!event) {
+      interaction.reply(`Could not find an event with code ${code}`);
+      return;
+    }
+
+    if (hardDelete) {
+      await event.delete();
+      interaction.reply(
+        `The event \`${event.name}\` was hard deleted. This cannot be reversed.`
+      );
+      return;
+    }
+
+    if (!event.active) {
+      interaction.reply(`This event was already inactive.`);
+      return;
+    }
+
+    event.active = false;
+    await event.save();
+    interaction.reply(
+      `Event ${event.name} has been marked as inactive, and can no longer be attended. There were ${event.attendees.length} attendees.`
+    );
+  },
+};
