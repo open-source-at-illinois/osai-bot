8c8af2bc16e5ef4a926f22192a1333648c7c67df
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-24 22:55:40 -0500

    Add typing for Event objects

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,43 +1,56 @@
-import mongoose = require('mongoose')
+import mongoose = require("mongoose");
 
-const eventSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    code: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    when: {
-        type: String,
-        required: false,
-    },
-    where: {
-        type: String,
-        required: false,
-    },
-    points: {
-        type: Number,
-        required: true,
-        default: 1,
-    },
-    active: {
-        type: Boolean,
-        required: true,
-        default: true,
-    },
-    attendees: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-})
+export interface EventType {
+  name: string;
+  code: string;
+  description: string;
+  when: string;
+  where: string;
+  points: number;
+  active: boolean;
+  attendees: [mongoose.Types.ObjectId];
+}
 
-const Event = mongoose.model('Event', eventSchema)
+const eventSchema = new mongoose.Schema<EventType>({
+  name: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  when: {
+    type: String,
+    required: false,
+  },
+  where: {
+    type: String,
+    required: false,
+  },
+  points: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Event = mongoose.model("Event", eventSchema);
 
 export default Event;
