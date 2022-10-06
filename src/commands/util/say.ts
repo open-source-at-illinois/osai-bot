056388a29e67087a66e237273445653280b6e7d5
Soham Kulkarni <sohamkrox@gmail.com>
2022-10-05 22:43:19 -0500

    Fix /say command and make it exec-only

--- src/commands/util/say.ts
+++ src/commands/util/say.ts
@@ -1,19 +1,28 @@
-import { CommandInteraction } from "discord.js";
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { isExec } from "../../utils";
+import { NOT_AUTHORIZED_MESSAGE } from "../../constants";
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('say')
-        .setDescription('Makes the bot say something in text')
-        .addStringOption((option) => option.setName('text').setDescription('What the bot has to say')),
+  data: new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("Makes the bot say something in text")
+    .addStringOption((option) =>
+      option.setName("text").setDescription("What the bot has to say")
+    ),
 
-    async execute(interaction: CommandInteraction) {
-        // Get user input for the text
-        let text = interaction.options.getString('text');
-        // Text should be empty by default
-        if (text === null) {
-            text = "";
-        }
-        await interaction.reply(text);
-    },
+  async execute(interaction: CommandInteraction) {
+    // Get user input for the text
+    let text = interaction.options.getString("text");
+    // Text should be empty by default
+    if (text === null) {
+      text = "";
+    }
+    if (!isExec(interaction.member as GuildMember)) {
+      interaction.reply(NOT_AUTHORIZED_MESSAGE);
+      return;
+    }
+    await interaction.channel.send(text);
+    interaction.reply({ content: "Spoken!", ephemeral: true });
+  },
 };
